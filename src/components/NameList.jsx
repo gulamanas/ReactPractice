2e21c0b48c9e76a2d621d89a6d45506bba8640bf
Gulam Anas <g.anas786@gmail.com>
2021-12-03 23:07:51 +0530

    extended version of list rendering

--- src/components/NameList.jsx
+++ src/components/NameList.jsx
@@ -2,11 +2,42 @@ import React from 'react'
 
 function NameList() {
 
-    const names = ['Stark', 'Thor', 'Rogers', 'Banner']
-    const nameList = names.map(name => <h2>{name}</h2>)
+    // const names = ['Stark', 'Thor', 'Rogers', 'Banner']
+    const persons = [
+        {
+            id: 1,
+            name: 'Stark',
+            age: 30,
+            skill: 'React'
+        },
+        {
+            id: 2,
+            name: 'Thor',
+            age: 28,
+            skill: 'Angular'
+        },
+        {
+            id: 3,
+            name: 'Rogers',
+            age: 25,
+            skill: 'Vue'
+        },
+        {
+            id: 4,
+            name: 'Banner',
+            age: 32,
+            skill: 'Svelte'
+        },
+    ]
+    const personList = persons.map(person => <h2> I am {person.name}. My age is {person.age} years old. I am skilled in {person.skill} </h2>)
+    // const nameList = names.map(name => <h2>{name}</h2>)
     return (
         <div>
-            {nameList}
+            {personList}
+            {/* {
+                names.map(name => <h2>{name}</h2>)
+            } */}
+            {/* {nameList} */}
         </div>
     )
 }
