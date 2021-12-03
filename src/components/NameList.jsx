7032af19f593157ae275e17acd4eb0ede6e46558
Gulam Anas <g.anas786@gmail.com>
2021-12-03 23:29:09 +0530

    extended version of list rendering

--- src/components/NameList.jsx
+++ src/components/NameList.jsx
@@ -33,7 +33,7 @@ function NameList() {
 
     // const nameList = names.map(name => <h2>{name}</h2>)
     // const personList = persons.map(person => <h2> I am {person.name}. My age is {person.age} years old. I am skilled in {person.skill} </h2>)
-    const personList = persons.map(person => <Person person={person} />)
+    const personList = persons.map(person => <Person key={person.id} person={person} />)
 
     return (
         <div>
