fe5e34cf169c2bb89a62f05d960f9ca62be6bfee
Gulam Anas <g.anas786@gmail.com>
2021-12-04 22:01:32 +0530

    Index as keys

--- src/components/NameList.jsx
+++ src/components/NameList.jsx
@@ -3,45 +3,45 @@ import Person from './Person'
 
 function NameList() {
 
-    // const names = ['Stark', 'Thor', 'Rogers', 'Banner']
-    const persons = [
-        {
-            id: 1,
-            name: 'Stark',
-            age: 30,
-            skill: 'React'
-        },
-        {
-            id: 2,
-            name: 'Thor',
-            age: 28,
-            skill: 'Angular'
-        },
-        {
-            id: 3,
-            name: 'Rogers',
-            age: 25,
-            skill: 'Vue'
-        },
-        {
-            id: 4,
-            name: 'Banner',
-            age: 32,
-            skill: 'Svelte'
-        },
-    ]
+    const names = ['Stark', 'Thor', 'Rogers', 'Banner']
+    // const persons = [
+    //     {
+    //         id: 1,
+    //         name: 'Stark',
+    //         age: 30,
+    //         skill: 'React'
+    //     },
+    //     {
+    //         id: 2,
+    //         name: 'Thor',
+    //         age: 28,
+    //         skill: 'Angular'
+    //     },
+    //     {
+    //         id: 3,
+    //         name: 'Rogers',
+    //         age: 25,
+    //         skill: 'Vue'
+    //     },
+    //     {
+    //         id: 4,
+    //         name: 'Banner',
+    //         age: 32,
+    //         skill: 'Svelte'
+    //     },
+    // ]
 
     // const nameList = names.map(name => <h2>{name}</h2>)
     // const personList = persons.map(person => <h2> I am {person.name}. My age is {person.age} years old. I am skilled in {person.skill} </h2>)
-    const personList = persons.map(person => <Person key={person.id} person={person} />)
-
+    // const nameList = names.map((name, index) => <Person key={index} index={index} name={name} />)
+    const nameList = names.map((name, index) => <h2 key={index}>{index} {name}</h2>)
     return (
         <div>
-            {personList}
+            {/* {personList} */}
             {/* {
                 names.map(name => <h2>{name}</h2>)
             } */}
-            {/* {nameList} */}
+            {nameList}
         </div>
     )
 }
