de6d59ed1418eb929df99f40d8a48b32fd61a70d
Gulam Anas <g.anas786@gmail.com>
2021-12-12 15:08:06 +0530

    Higher order functions part 1

--- src/App.js
+++ src/App.js
@@ -21,12 +21,16 @@ import InputParent from './components/InputParent';
 import FRInputParent from './FRInputParent';
 import Hero from './components/Hero';
 import ErrorBoundary from './components/ErrorBoundary';
+import ClickCounter from './components/ClickCounter';
+import HoverCounter from './components/HoverCounter';
 
 
 function App() {
   return (
     <div className="App ">
-      <ErrorBoundary>
+      <ClickCounter />
+      <HoverCounter />
+      {/* <ErrorBoundary>
         <Hero heroName="Batman" />
       </ErrorBoundary>
       <ErrorBoundary>
@@ -34,7 +38,7 @@ function App() {
       </ErrorBoundary>
       <ErrorBoundary>
         <Hero heroName="Joker" />
-      </ErrorBoundary>
+      </ErrorBoundary> */}
       {/* <FRInputParent /> */}
       {/* <InputParent /> */}
       {/* <RefsDemo /> */}
